64254a66752a7bcff872a4ea81c5d408bf27e869
CMSystem-0 <148085754+CMSystem-0@users.noreply.github.com>
2024-01-14 16:46:17 +0530

    Add files via upload

---
+++ mcsys/src/main/java/com/manageclgsys/springboot/mcsys/McsysApplication.java
@@ -0,0 +1,13 @@
+package com.manageclgsys.springboot.mcsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsysApplication.class, args);
+	}
+
+}
